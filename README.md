f7b8913e3167001c8f9fefc52db45274938e9035
kimgangmoon <rkdans2111@naver.com>
2025-09-17 00:22:07 +0900

    feat:right section

--- README.md
+++ README.md
@@ -457,3 +457,7 @@ filter, all, /:post_id offset과 limit 추가 필요.
 ### 9/15
 
 1. 쉬었습니다. 죄송합니다.
+
+### 9/16
+
+1. right section 작성중.
--- frontend/app/component/rightSection.tsx
+++ frontend/app/component/rightSection.tsx
@@ -10,6 +10,7 @@ import {
   PopoverPanel,
 } from "@headlessui/react";
 import Image from "next/image";
+import Link from "next/link";
 import { useId, useState } from "react";
 
 export const RightSection = () => {
@@ -17,11 +18,11 @@ export const RightSection = () => {
   const [text, setText] = useState("");
   const [isFocus, setIsFocus] = useState(false);
   return (
-    <section className="flex flex-col justify-between w-2/7 pr-2 h-full select-none border-l border-Lgray">
-      <div className="relative w-full h-10 mx-4">
+    <section className="flex flex-col w-3/7 h-full pl-4 border-l border-Lgray">
+      <div className="relative w-full h-10">
         <label
           className={
-            "relative pl-10 flex items-center w-full h-full border border-Dgray rounded-3xl"
+            "relative pl-10 flex items-center w-full h-full border border-Dgray rounded-3xl select-none"
           }
           htmlFor={inputId}
           onFocus={() => setIsFocus(true)}
@@ -48,27 +49,88 @@ export const RightSection = () => {
         </label>
         <div
           hidden={!isFocus}
-          className="absolute top-full left-0 w-full h-auto rounded-2xl overflow-hidden shadow-md p-4"
+          className="absolute top-full left-0 w-full h-auto rounded-2xl overflow-hidden shadow-md p-4 bg-white"
         >
-          <ul>
-            <li className="flex">
-              <span className="block relative w-12 aspect-square">
-                <Image
-                  className="rounded-full"
-                  src={"userImg.svg"}
-                  alt="userImg.svg"
-                  style={{ objectFit: "fill" }}
-                  fill
-                ></Image>
-              </span>
-              <span className="block text-sm w-full pl-2">
-                <span className="block text-left font-bold">김강문</span>
-                <span className="block text-left text-Dgray">@SSD</span>
-              </span>
-            </li>
+          <ul className="grid grid-rows-1 gap-4">
+            <SearchItem name="김강문" email="SSD" href=""></SearchItem>
+            <SearchItem name="김강문" email="SSD" href=""></SearchItem>
+            <SearchItem name="김강문" email="SSD" href=""></SearchItem>
+            <SearchItem name="김강문" email="SSD" href=""></SearchItem>
           </ul>
         </div>
       </div>
+      <ArticlesSection></ArticlesSection>
     </section>
+  );
+};
+
+const SearchItem = ({
+  name,
+  email,
+  href,
+}: {
+  name: string;
+  email: string;
+  href: string;
+}) => {
+  return (
+    <li>
+      <Link href={href} className="flex">
+        <span className="block relative w-12 aspect-square">
+          <Image
+            className="rounded-full"
+            src={"userImg.svg"}
+            alt="userImg.svg"
+            style={{ objectFit: "fill" }}
+            fill
+          ></Image>
+        </span>
+        <span className="block text-sm w-full pl-2">
+          <span className="block text-left font-bold">{name}</span>
+          <span className="block text-left text-Dgray">@{email}</span>
+        </span>
+      </Link>
+    </li>
+  );
+};
+
+const ArticlesSection = () => {
+  return (
+    <div className="p-4 border-1 border-Lgray rounded-2xl mt-5">
+      <h3 className="text-xl font-bold">What's happening</h3>
+      <ul className="grid grid-rows-1 gap-2">
+        <ArticleItem name="김강문" email="ssd" href=""></ArticleItem>
+      </ul>
+    </div>
+  );
+};
+
+const ArticleItem = ({
+  name,
+  email,
+  href,
+}: {
+  name: string;
+  email: string;
+  href: string;
+}) => {
+  return (
+    <li>
+      <Link href={href} className="flex">
+        <span className="block relative w-12 aspect-square">
+          <Image
+            className="rounded-full"
+            src={"userImg.svg"}
+            alt="userImg.svg"
+            style={{ objectFit: "fill" }}
+            fill
+          ></Image>
+        </span>
+        <span className="block text-sm w-full pl-2">
+          <span className="block text-left font-bold">{name}</span>
+          <span className="block text-left text-Dgray">@{email}</span>
+        </span>
+      </Link>
+    </li>
   );
 };
